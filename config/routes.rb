1150a770ad01620dcd07532a7a560de734480f46
Laura Whalin <laurawhalin@gmail.com>
2015-04-20 17:23:33 -0600

    added all anticipated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,15 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root "home#index"
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :home, only: [:edit, :update]
+  get "home", to: "home#index"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :learn_more, only: [:edit, :update]
+  get "learn_more", to: "learn_more#index"
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  resources :tools_resources, only: [:edit, :update]
+  get "tools_resources", to: "tools_resources#index"
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :companies, only: [:create]
+  get 'signup', to: "companies#new"
 end
