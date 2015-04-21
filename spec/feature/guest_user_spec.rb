cb392b24a9c713a456335550fb449498b5e36449
Chris Luhring <cluhring@gmail.com>
2015-04-20 23:38:32 -0600

    passing signup form feature test

---
+++ spec/feature/guest_user_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "As a guest user" do
+  include Capybara::DSL
+
+  it "can see a form on the signup page" do
+    visit ('/signup')
+    within(".home-signup_form") do
+        expect(page).to have_content("Start hiring")
+      within(".signup") do
+        expect(page).to have_content("Name (First and Last)")
+        expect(page).to have_content("Organization")
+        expect(page).to have_content("Title")
+        expect(page).to have_content("State")
+        expect(page).to have_content("City")
+        expect(page).to have_content("Email")
+        find_button("Create techHire account")
+      end
+    end
+  end
+end
