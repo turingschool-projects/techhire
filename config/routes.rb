b448c078e3d55de56efe43b6d0c174c98d21bb2f
Chris Luhring <cluhring@gmail.com>
2015-04-20 23:56:38 -0600

    added home create route to accommodate signup form on homepage w/ feature test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
-  resources :home, only: [:index, :edit, :update]
+  resources :home, only: [:index, :edit, :update, :create]
   resources :learn_more, only: [:index, :edit, :update]
   resources :tools_resources, only: [:index, :edit, :update]
   resources :companies, only: [:create]
