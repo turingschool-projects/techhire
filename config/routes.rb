50a8d61caa1981d52b4917e37eb17a3ab7e61482
Laura Whalin <laurawhalin@gmail.com>
2015-04-20 21:01:55 -0600

    removed weird index treatments on the controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
-
-  resources :home, only: [:edit, :update]
-  get "home", to: "home#index"
-
-  resources :learn_more, only: [:edit, :update]
-  get "learn_more", to: "learn_more#index"
-
-  resources :tools_resources, only: [:edit, :update]
-  get "tools_resources", to: "tools_resources#index"
-
+  resources :home, only: [:index, :edit, :update]
+  resources :learn_more, only: [:index, :edit, :update]
+  resources :tools_resources, only: [:index, :edit, :update]
   resources :companies, only: [:create]
   get 'signup', to: "companies#new"
 end
