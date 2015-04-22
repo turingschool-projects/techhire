5b86c309196f2a86c747885db98b28cef3732dc2
KristaANelson <krista.a.nelson@gmail.com>
2015-04-22 14:13:23 -0600

    update the routes to be more URL friendly and match the specs. Move map to Techhire locations page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#home"
   get "/home", to: "static_pages#home", as: "home"
-  get "/learn_more", to: "static_pages#learn_more", as: "learn_more"
-  get "/tools_resources", to: "static_pages#tools_resources", as: "tools_resources"
-  get "/locations", to: "static_pages#locations", as: "locations"
+  get "/learn-more", to: "static_pages#learn_more", as: "learn_more"
+  get "/tools-resources", to: "static_pages#tools_resources", as: "tools_resources"
+  get "/techhire-locations", to: "static_pages#locations", as: "techhire_locations"
   resources :companies, only: [:create]
   get 'signup', to: "companies#new"
 end
