0798f0f180876e3aa4b9d72f6030f67e07168388
Laura Whalin <laurawhalin@gmail.com>
2015-04-21 21:38:21 -0600

    changed routes so that static pages are on one controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root "home#index"
-  resources :home, only: [:index, :edit, :update]
-  resources :learn_more, only: [:index, :edit, :update]
-  resources :tools_resources, only: [:index, :edit, :update]
+  root "static_pages#home"
+  get "/home", to: "static_pages#home", as: "home"
+  get "/learn_more", to: "static_pages#learn_more", as: "learn_more"
+  get "/tools_resources", to: "static_pages#tools_resources", as: "tools_resources"
+  get "/locations", to: "static_pages#locations", as: "locations"
   resources :companies, only: [:create]
   get 'signup', to: "companies#new"
 end
