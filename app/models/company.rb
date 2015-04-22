939c464379df9e304bf19bd3351563e5591d94bc
Chris Luhring <cluhring@gmail.com>
2015-04-22 13:42:43 -0600

    validating presence of Company attributes and email format

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,5 +2,5 @@ class Company < ActiveRecord::Base
   validates :name, :organization, :title, :state, :city, presence: true
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, }
-
+  validates_numericality_of :hire_count
 end
