66b42db1356ef63a08a25b54db38518ad18aa679
Orion Osborn <orion.osborn@gmail.com>
2015-04-23 16:41:59 -0600

    add authorize! and current_user methods to ensure only admins can view dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,16 @@
 class Admin::DashboardController < ApplicationController
+  before_action :authorize!
+
   def index
     @uncontacted_companies = Company.where(status: "uncontacted")
     @contacted_companies = Company.where(status: "contacted")
+  end
+
+  private
+
+  def authorize!
+    unless current_user.admin?
+      redirect_to root_path
+    end
   end
 end
