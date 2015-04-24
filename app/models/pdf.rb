48b86ad5027dd7c74d0ee65299ce514f3f58295c
Jeffrey Wan <Jwan622@gmail.com>
2015-04-24 09:29:43 -0600

    pdf upload works on s3

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -1,17 +1,23 @@
 class Pdf < ActiveRecord::Base
   has_attached_file :pdf_file,
-    :path => ":rails.root/public/assets/pdfs/:id.:extension"
+    :path => "public/assets/pdfs/:basename.:extension"
 
   validates_attachment_content_type :pdf_file,
                                     :content_type => [ 'application/pdf' ],
                                     :message => "only pdf files are allowed",
                                     :if => :pdf_attached?
 
+  before_save :add_page
+
   def self.learn_more
-    where("page = 'tools and resources' and slot = 1").first
+    where("page = 'learn more' and slot = 1").first
   end
 
   def pdf_attached?
     self.pdf_file.present?
+  end
+
+  def add_page
+    self.page ||= "learn more"
   end
 end
