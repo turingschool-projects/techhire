7eeda8b7b06ca35f8de515c2410d3e298f3c83b0
Jeffrey Wan <Jwan622@gmail.com>
2015-04-25 15:59:38 -0600

    fixed deprecation for controller test

--- spec/controllers/pdfs_controller_spec.rb
+++ spec/controllers/pdfs_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe PdfsController, type: :controller do
   it "should respond back with a pdf" do
-    Paperclip::Attachment.any_instance.stub(:url).and_return("spec/fixtures/sample_pdf.pdf")
+    allow_any_instance_of(Paperclip::Attachment).to receive(:url).and_return("spec/fixtures/sample_pdf.pdf")
 
     pdf_file = Rack::Test::UploadedFile.new('spec/fixtures/sample_pdf.pdf', 'application/pdf')
     pdf = create(:pdf, pdf_file: pdf_file)
@@ -11,5 +11,4 @@ RSpec.describe PdfsController, type: :controller do
     expect(response.headers["Content-Type"]).to eql("application/pdf")
     expect(response.headers["Content-Disposition"]).to match(/sample.pdf.pdf/)
   end
-
 end
