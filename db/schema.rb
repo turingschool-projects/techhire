46e5c2e95b8c1ccbd0011338d6732cf09bf42705
Jeffrey Wan <Jwan622@gmail.com>
2015-04-27 13:21:08 -0600

    fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150422201652) do
+ActiveRecord::Schema.define(version: 20150425174508) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,4 +50,5 @@ ActiveRecord::Schema.define(version: 20150422201652) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
   end
+
 end
