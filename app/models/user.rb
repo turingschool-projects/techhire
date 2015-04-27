506f22684c613d88b19bfa10223064e8d8d01de4
Orion Osborn <orion.osborn@gmail.com>
2015-04-27 13:09:16 -0600

    add super_admin user to enum roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  enum role: %w(company admin)
+  enum role: %w(company admin super_admin)
 
   def admin?
-    role == "admin"
+    role == "admin" || role == "super_admin"
   end
 end
