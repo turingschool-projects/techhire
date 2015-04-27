41e6a3114184622e9161fdbb9eb98616d6186703
Lydias303 <shaferlydia@yahoo.com>
2015-04-27 14:09:30 -0600

    added tests for manage content links

--- spec/features/admin_dashboard_spec.rb
+++ spec/features/admin_dashboard_spec.rb
@@ -18,27 +18,96 @@ RSpec.feature "AdminDashboard", type: :feature do
       click_link_or_button('Login')
     end
 
-   it "views uncontacted companies on dashboard" do
-      companies = create_list(:company, 10, status: "uncontacted")
+   it "can click a link to view and manage the companies page" do
       login
 
       expect(current_path).to eq(admin_dashboard_index_path)
 
-      within(".uncontacted-companies") do
-        expect(page).to have_content(companies.first.organization)
-        expect(page).to have_content(companies.last.organization)
+      within('.navbar-header') do
+        expect(page).to have_content('Companies')
+        click_link_or_button('Companies')
+      end
+
+      expect(current_path).to eq(admin_companies_path)
+    end
+
+    it "can click a link to manage static content" do
+      login
+
+      expect(current_path).to eq(admin_dashboard_index_path)
+
+      within("li.manage-static-link") do
+        expect(page).to have_content('Manage Static Content')
       end
     end
 
-    it "views contacted companies on dashboard" do
-      companies = create_list(:company, 10, status: "contacted")
+    it "can click a link to manage home content" do
       login
 
       expect(current_path).to eq(admin_dashboard_index_path)
 
-      within(".contacted-companies") do
-        expect(page).to have_content(companies.first.organization)
-        expect(page).to have_content(companies.last.organization)
+      within("li.manage-home") do
+        expect(page).to have_content('Home')
+      end
+    end
+
+    it "can click a link to manage map content" do
+      login
+
+      expect(current_path).to eq(admin_dashboard_index_path)
+
+      within("li.manage-map") do
+        expect(page).to have_content('Manage Map')
+      end
+    end
+
+    it "can click a link to manage PDF files content" do
+      login
+
+      expect(current_path).to eq(admin_dashboard_index_path)
+
+      within("li.manage-pdf") do
+        expect(page).to have_content('Manage PDF Files')
+      end
+    end
+
+    it "can click a link to manage video content" do
+      login
+
+      expect(current_path).to eq(admin_dashboard_index_path)
+
+      within("li.manage-video") do
+        expect(page).to have_content('Manage Video Files')
+      end
+    end
+
+    it "can click a link to manage tools and resources content" do
+      login
+
+      expect(current_path).to eq(admin_dashboard_index_path)
+
+      within("li.manage-tools") do
+        expect(page).to have_content('Tools/Resources')
+      end
+    end
+
+    it "can click a link to manage Learn More content" do
+      login
+
+      expect(current_path).to eq(admin_dashboard_index_path)
+
+      within("li.manage-learn") do
+        expect(page).to have_content('Learn More')
+      end
+    end
+
+    it "can click a link to manage sign up content" do
+      login
+
+      expect(current_path).to eq(admin_dashboard_index_path)
+
+      within("li.manage-sign-up") do
+        expect(page).to have_content('Sign Up')
       end
     end
   end
