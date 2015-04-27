2886d5557200974c43015344118c9857d8516f39
Lydias303 <shaferlydia@yahoo.com>
2015-04-27 14:26:24 -0600

    changed search boxes to match the model

--- app/controllers/admin/companies_controller.rb
+++ app/controllers/admin/companies_controller.rb
@@ -1,6 +1,7 @@
 class Admin::CompaniesController < ApplicationController
   def index
-    @uncontacted_companies = Company.where(status: "uncontacted")
-    @contacted_companies = Company.where(status: "contacted")
+    @companies = Company.all
+    # @uncontacted_companies = Company.where(status: "uncontacted")
+    # @contacted_companies = Company.where(status: "contacted")
   end
 end
