b5c3ffe718a7320d1e918925853864d5cd063411
Alex Robinson <alexandra.scott335@gmail.com>
2015-04-28 17:17:44 -0600

    add enum roles with devise and fixed failing tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
-
   def after_sign_in_path_for(resource)
-    admin_dashboard_index_path
+    if current_user && current_user.admin?
+      admin_dashboard_index_path
+    else
+      root_path
+    end
   end
 end
--- spec/features/admin_logins_spec.rb
+++ spec/features/admin_logins_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "AdminLogins", type: :feature do
   describe("an admin can login") do
     let(:admin) do
-      User.create(email: 'admin@example.com', password: 'password')
+      User.create(email: 'admin@example.com', password: 'password', role: 1)
     end
 
     it "visits login" do
@@ -19,23 +19,22 @@ RSpec.feature "AdminLogins", type: :feature do
     it "doesn't allow non-users to login" do
       visit new_user_session_path
 
-      fill_in 'user[email]', with: admin.email
-      fill_in 'user[password]', with: admin.password
+      fill_in 'user[email]', with: 'test@example.com'
+      fill_in 'user[password]', with: 'password'
       click_link_or_button('Log in')
 
-      expect(current_path).to eq(admin_login_path)
+      expect(current_path).to eq(new_user_session_path)
     end
 
     it "doesn't allow non-admin users to login and view admin dashboard" do
       user = User.create(email: 'user@example.com', password: 'password')
       visit new_user_session_path
 
-      fill_in 'user[email]', with: admin.email
-      fill_in 'user[password]', with: admin.password
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
       click_link_or_button('Log in')
 
-      expect(current_path).to_not eq(admin_login_path)
-      expect(current_path).to_not eq(admin_dashboard_index_path)
+      expect(current_path).to eq(root_path)
     end
   end
 end
