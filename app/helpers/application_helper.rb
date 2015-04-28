75b9bf7fd6549d9a4493fc8c9058c2cf55ab2e88
Chris Luhring <cluhring@gmail.com>
2015-04-28 16:06:16 -0600

    deleted city helper method, no longer needed thanks to grouped_collection_select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,4 @@ module ApplicationHelper
     alphastates = State.all.map { |state| state.abbr }
     return alphastates
   end
-
-  def city_helper
-    raise "boom"
-    all_cities = UsaCity.all.map { |city| city.name }
-    return all_cities.reverse
-  end
 end
