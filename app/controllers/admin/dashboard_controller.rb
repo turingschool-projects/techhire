02a50dc06ccdc4183afe1e01a6361142d77e3b9a
Alex Robinson <alexandra.scott335@gmail.com>
2015-04-28 17:25:04 -0600

    added authorization for admin dashboard and admin companies controllers

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,15 +1,7 @@
 class Admin::DashboardController < ApplicationController
-  #before_action :authorize!
+  before_action :authorize!
 
   def index
-    
-  end
 
-  private
-
-  #def authorize!
-    #unless current_user.admin?
-      #redirect_to root_path
-    #end
-  #end
+  end  
 end
