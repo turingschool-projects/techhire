2c785d7e7b952d17f044cd177a5fe65cb6d81847
Jeffrey Wan <Jwan622@gmail.com>
2015-04-29 14:20:52 -0600

    merged conflcits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428225710) do
+ActiveRecord::Schema.define(version: 20150429043602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,9 @@ ActiveRecord::Schema.define(version: 20150428225710) do
     t.integer  "role",                   default: 0
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
   create_table "videos", force: :cascade do |t|
     t.string   "url"
     t.string   "page"
@@ -84,9 +87,6 @@ ActiveRecord::Schema.define(version: 20150428225710) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "notes", "companies"
 end
