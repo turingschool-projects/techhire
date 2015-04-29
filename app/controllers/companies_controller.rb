5740c35dac23f1af9da7eb6bc909c66ae4b0aee7
Orion Osborn <orion.osborn@gmail.com>
2015-04-29 15:00:32 -0600

    Create users and companies when someone fills in signup form
    There are some broken test

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,9 +7,14 @@ class CompaniesController < ApplicationController
   end
 
   def create
-    user = User.create(email: company_params[:email], name: company_params[:name], title: company_params[:title], password: User.generate_password)
     company = Company.new(company_params)
     if company.save
+      User.create(email: company_params[:email],
+                         name: company_params[:name],
+                         title: company_params[:title],
+                         password: User.generate_password,
+                         company_id: company.id
+                        )
       flash[:success] = "Welcome #{company.name}"
       redirect_to company_path(company.id)
     else
