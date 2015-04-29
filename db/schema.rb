d7fca8b011cf521c983bcb1141c2b375b16f8ce4
Jeffrey Wan <Jwan622@gmail.com>
2015-04-28 22:46:57 -0600

    added application.yml back

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-''# encoding: UTF-8
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428141114) do
->>>>>>> e7a8c996703873eb9d447072dda56172e30fbf81
+ActiveRecord::Schema.define(version: 20150429043602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +56,8 @@ ActiveRecord::Schema.define(version: 20150428141114) do
     t.string   "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   create_table "users", force: :cascade do |t|
