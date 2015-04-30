9c002a8603e58771cf14afeb79602219add644fd
Jeffrey Wan <Jwan622@gmail.com>
2015-04-29 18:23:17 -0600

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,28 @@ Rails.application.routes.draw do
   devise_for :users
   root "static_pages#home"
   get "/home", to: "static_pages#home", as: "home"
-  get "/learn-more", to: "static_pages#learn_more", as: "learn_more"
-  get "/tools-resources", to: "static_pages#tools_resources", as: "tools_resources"
-  get "/techhire-locations", to: "static_pages#techhire_locations", as: "techhire_locations"
+  get "/learn_more", to: "static_pages#learn_more", as: "learn_more"
+  get "/tools_resources", to: "static_pages#tools_resources", as: "tools_resources"
+  get "/techhire_locations", to: "static_pages#techhire_locations", as: "techhire_locations"
   get '/admin', to: 'sessions#new', as: 'admin/login'
   post '/admin', to: 'sessions#create'
   get '/download', to: "static_pages#download", as: "download"
 
   namespace :admin do
     resources :dashboard, only: [:index]
+    resources :contents
+    resources :videos
+    resources :pdfs
+    resources :techhire_locations
     resources :companies, only: [:show, :destroy, :update, :index] do
       resources :notes, only: [:index, :create, :destroy]
     end
-    resources :pdfs, only: [:create, :new]
-    resource :tools_resources, only: [:edit]
+    namespace :pages do
+      resource :tools_resources, only: [:edit]
+      resource :learn_more, only: [:edit]
+      resource :home, only: [:edit]
+      resource :techhire_locations, only: [:edit]
+    end
   end
 
   resources :companies, only: [:create]
