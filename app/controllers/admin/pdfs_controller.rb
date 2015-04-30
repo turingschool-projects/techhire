27d420e9aa71b827cbb874ec6a855c69a245b57c
Chris Luhring <cluhring@gmail.com>
2015-04-30 15:07:10 -0600

    added pdf index/delete functions to admin/pdf page w/ tests

--- app/controllers/admin/pdfs_controller.rb
+++ app/controllers/admin/pdfs_controller.rb
@@ -2,16 +2,34 @@ class Admin::PdfsController < ApplicationController
   def new
   end
 
+  def index
+    @pdfs = Pdf.all
+  end
+
+  def show
+    pdf = Pdf.find(params[:id])
+    data = open("#{pdf.pdf_file.url}")
+    send_data data.read, filename: "#{pdf.pdf_file_file_name}", type: "application/pdf", disposition: 'inline', stream: 'true', buffer_size: '4096'
+    # redirect_to pdf.pdf_file.url
+  end
+
   def create
     pdf = Pdf.new(new_pdf_params)
     if pdf.save
       flash[:success] = "Pdf successfully uploaded"
-      redirect_to admin_dashboard_index_path
+      redirect_to admin_pdfs_path
     else
       flash[:error] = "Something went wrong with the upload"
       redirect_to edit_admin_tools_resources_path
     end
   end
+
+  def destroy
+    Pdf.find(params[:id]).destroy
+    redirect_to admin_pdfs_path
+  end
+
+  private
 
   def new_pdf_params
     params.require(:pdf).permit(:pdf_file, :slot)
--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -1,3 +1,4 @@
+require 'time'
 class Pdf < ActiveRecord::Base
   has_attached_file :pdf_file,
     :path => "public/assets/pdfs/:basename.:extension",
@@ -14,5 +15,9 @@ class Pdf < ActiveRecord::Base
 
   def pdf_attached?
     self.pdf_file.present?
+  end
+
+  def update_formatter
+    time = self.pdf_file_updated_at.strftime("%b %e, %l:%M %p")
   end
 end
