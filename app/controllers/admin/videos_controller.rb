71e29586da8ce6d80a4f37bdd192ce477b758d5b
Jeffrey Wan <Jwan622@gmail.com>
2015-04-29 20:40:06 -0600

    added admin/videos page

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,2 +1,18 @@
 class Admin::VideosController < ApplicationController
+  def create
+    video = Video.new(video_params)
+    if video.save
+      flash[:notice] = "Video succesfully uploaded"
+      redirect_to admin_videos_path
+    else
+      flash[:error] = video.errors.messages[:error][0]
+      render :index
+    end
+  end
+
+  private
+
+  def video_params
+    params.require(:video).permit(:url, :title)
+  end
 end
--- spec/factories/techhire_locations.rb
+++ spec/factories/techhire_locations.rb
@@ -2,4 +2,5 @@ FactoryGirl.define do
   factory :techhire_location do
     city "New York City"
     state "NY"
+  end
 end
