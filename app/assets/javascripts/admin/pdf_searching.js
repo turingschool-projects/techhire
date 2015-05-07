5aef847f4c3062ac56c9ea6251e032540909cb81
worace <horace.d.williams@gmail.com>
2015-05-06 19:22:36 -0600

    move misnamed jquery.js to separate js file

---
+++ app/assets/javascripts/admin/pdf_searching.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  function showBySearchTerm(listLinks, searchMatch){
+    listLinks.each(function (index, element){
+      if($(element).text().match(searchMatch)) {
+        $(element).show();
+      } else {
+        $(element).hide();
+      }
+    });
+  }
+
+  function showAll(listLinks, searchMatch){
+    listLinks.each(function (index, element){
+      $(element).show();
+    });
+  }
+
+  $('#filter-pdfs').click(function(){
+    var filterBy = $('#pdf_file_name').val()
+    var $listLinks = $('.all-pdfs .PDFs')
+    var searchMatch = new RegExp(filterBy, "i")
+    if($(this).val()==='Filter'){
+      showBySearchTerm($listLinks, searchMatch);
+      $(this).val('Show All');
+    }else{
+      showAll($listLinks, searchMatch);
+      $(this).val('Filter');
+    }
+  });
+});
