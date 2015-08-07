da533fd0f1395b92e1ad24bf33b3f80734e8acd3
Timothy Mee <tjmee90@gmail.com>
2015-08-07 10:10:46 -0600

    setup new relations between, content, page, and pdf models.  PDF might be changed depending on how the CMS will work show the pdf content

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,9 +1,14 @@
 class Content < ActiveRecord::Base
-  validates_length_of :title, within: 10..60,
-                              too_short: "The min title length is 10 characters.",
-                              too_long: "The max title length is 60 characters."
-  validates :title, uniqueness: { message: "There cannot be duplicate titles." }
-
   validates :body, presence: { message: "The body can't be blank." },
-                  uniqueness: { message: "There cannot be duplicate bodies." }
+                   uniqueness: { message: "There cannot be duplicate bodies." }
+
+  belongs_to :page
+  has_one :pdf
+
+
+  def self.null(location=1)
+    new location: location, body: "Not yet set"
+  end
+
 end
+
