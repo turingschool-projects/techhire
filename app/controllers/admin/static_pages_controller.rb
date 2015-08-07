d79b476f66c617cb46b82f375ea455888932d625
Timothy Mee <tjmee90@gmail.com>
2015-08-07 10:19:36 -0600

    static view layout

---
+++ app/controllers/admin/static_pages_controller.rb
@@ -0,0 +1,24 @@
+class Admin::StaticPagesController < ApplicationController
+  before_action :authorize!
+
+  def edit
+    name  = clean_params(params)
+    @page = Page.find_by(name: name)
+    @contents = @page.contents.order("location")
+  end
+
+  private
+
+  def clean_params(params)
+    case params[:name]
+    when "learn_more"
+      name = "Learn More"
+    when "home"
+      name = "Home"
+    when "tools_resources"
+      name = "Tools and Resources"
+    when "techhire_locations"
+      name = "TechHire Locations"
+    end
+  end
+end
