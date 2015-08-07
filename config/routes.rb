b457ffcdfddc21280c5972123e77c494562b019d
Timothy Mee <tjmee90@gmail.com>
2015-08-07 10:14:38 -0600

    changed routes to work with new static controller show method

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,36 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root "static_pages#home"
-  get "/home", to: "static_pages#home", as: "home"
-  get "/learn-more", to: "static_pages#learn_more", as: "learn_more"
-  get "/tools-resources", to: "static_pages#tools_resources", as: "tools_resources"
-  get "/techhire-locations", to: "static_pages#techhire_locations", as: "techhire_locations"
-  get "/download", to: "static_pages#download", as: "download"
+  root "static_pages#show", name: "Home"
+  get "/home", to: "static_pages#show", as: "home", name: "Home"
+
+  get "/learn-more", to: "static_pages#show", as: "learn_more", name: "Learn More"
+  get "/tools-resources", to: "static_pages#show", as: "tools_resources", name: "Tools and Resources"
+  get "/techhire-locations", to: "static_pages#show", as: "techhire_locations", name: "TechHire Locations"
+
   get "/companies/welcome", to: "companies#welcome", as: "welcome"
   get 'signup', to: "companies#new", as: :signup
 
   resources :case_studies, only: [:show]
 
   namespace :admin do
+    get   "/static_pages/:name", to: "static_pages#edit",  as: :static_pages
+    put   "/static_pages/:name", to: "static_pages#update"
+    patch "/static_pages/:name", to: "static_pages#update"
+
     resources :dashboard, only: [:index]
     get "/dashboard/cms-instructions", to: "dashboard#cms_instructions", as: "cms_instructions"
     resources :contents
-    resources :videos
     resources :pdfs
     resources :techhire_locations
     resources :csv_uploads, only: [:new, :create]
     resources :companies, except: [:create] do
       resources :notes, only: [:index, :create, :destroy]
     end
-    namespace :static_pages do
-      get "/home", to: "static_pages#home", as: "home"
-      get "/learn_more", to: "static_pages#learn_more", as: "learn_more"
-      get "/tools_resources", to: "static_pages#tools_resources", as: "tools_resources"
-      get "/techhire_locations", to: "static_pages#techhire_locations", as: "techhire_locations"
-    end
   end
 
   resources :companies, only: [:create, :show]
   resources :pdfs, only: [:show]
-
-  get "/boom", to: "application#boom"
 
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
