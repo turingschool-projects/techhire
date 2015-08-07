15de03ae4c8c52d6ea28237fd950f8f6c77965d6
Timothy Mee <tjmee90@gmail.com>
2015-08-07 10:09:09 -0600

    changed static page controller to load pages through params and show method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,12 @@
 class StaticPagesController < ApplicationController
   layout "static_application"
 
-  def home
-  end
-
-  def learn_more
-  end
-
-  def tools_resources
-    @pdf_learn_more = Pdf.learn_more
-  end
-
-  def techhire_locations
-    @techhire_locations = TechhireLocation.all
+  def show
+    if params[:name].nil?
+      puts "\e[31mExpected params[:name] to be something like 'home', but it's nil!\e[0m"
+      p params
+    end
+    @page     = Page.find_by!(name: params[:name])
+    @template = StaticTemplateRenderer.new(@page).render
   end
 end
