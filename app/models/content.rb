166b7b291a0b704afc1cd092be09dfe51fa1c439
Timothy Mee <tjmee90@gmail.com>
2015-08-10 11:06:22 -0600

    whitespace

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,13 +1,10 @@
 class Content < ActiveRecord::Base
   validates :body, presence: { message: "The body can't be blank." },
                    uniqueness: { message: "There cannot be duplicate bodies." }
-
   belongs_to :page
-
 
   def self.null(location=1)
     new location: location, body: "Not yet set"
   end
 
 end
-
