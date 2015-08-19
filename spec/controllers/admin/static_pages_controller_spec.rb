9a13c27f92eb14f420a0fa5a89a5c3df61509b97
Timothy Mee <tjmee90@gmail.com>
2015-08-19 10:07:58 -0600

    added/changed specs for controllers

--- spec/controllers/admin/static_pages_controller_spec.rb
+++ spec/controllers/admin/static_pages_controller_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Admin::StaticPagesController, type: :controller do
+  let(:admin) do
+    User.create(email: "tim@example.com", password: "password", password_confirmation: "password", role: 2)
+  end
 
+  before(:each) do
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+  end
+
+  before :all do
+    Page.count > 0 ? Page.destroy_all : false
+    ['Home', 'Learn More', 'TechHire Locations', 'Tools and Resources'].each do |name|
+      Page.create! do |page|
+        page.number_of_locations = 3
+        page.name          = name
+        page.template      = <<-HTML.strip_heredoc
+          <div>
+            <h1>There is so much good content on this page</h1>
+          </div>
+        HTML
+      end
+    end
+  end
+
+  describe "Edit home" do
+    it "returns http success" do
+      get :edit, {:name => 'home'}
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "Edit learn_more" do
+    it "returns http success" do
+      get :edit, {:name => 'learn_more'}
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "Edit tools_resources" do
+    it "returns http success" do
+      get :edit, {:name => 'tools_resources'}
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "Edit techhire_locations" do
+    it "returns http success" do
+      get :edit, {:name => "techhire_locations"}
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
