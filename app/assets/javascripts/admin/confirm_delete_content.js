f878a47bbb07a8637c885ecc938c658eb9ec5aed
Timothy Mee <tjmee90@gmail.com>
2015-08-19 10:01:19 -0600

    deleted old js file and fixed company controller update method

--- app/assets/javascripts/admin/confirm_delete_content.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $(document).on('click', '.deleteBtn', function() {
-    var contentId = "contents/" + $(this).data('id');
-    $('.modal-footer #contentID').attr('href', contentId);
-  });
-});
