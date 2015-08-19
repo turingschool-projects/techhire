5b37ee2701f5238479b68befbf5b7fd48dd75bf8
Timothy Mee <tjmee90@gmail.com>
2015-08-19 10:10:27 -0600

    model spec

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,4 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
+  describe "null content" do
+    it "will assign a default location if null" do
+      content =  Content.null
+      expect(content.location).to eq(1)
+    end
+  end
 end
